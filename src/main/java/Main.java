969c3273a762435fb3d34e4c8f4d24a7cdc29d4a
Dmitrii Borovkov <33982125+f1sh-d0g@users.noreply.github.com>
2024-11-19 19:10:55 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Какая-то надпись");
         System.out.println("Другая надпись");
+        System.out.println("Ещё одна надпись");
     }
 }
