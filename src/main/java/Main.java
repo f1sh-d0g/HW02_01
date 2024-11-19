97316c8693f9e2573c438b2fba121f7dd45d6352
DorovkovDA <dmitry.borovkov@gmail.com>
2024-11-19 19:09:12 +0300

    HW2_01 second push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Пока 03?");
+        System.out.println("Четвертая ветка");
+        String s = "Надпись номер 1";
+
+        System.out.println("Четвертая ветка");
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+
+
     }
 }
