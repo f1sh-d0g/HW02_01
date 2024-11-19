0260c799c09aea02473ba6b7ce1fa554cff67e2b
Dmitrii Borovkov <33982125+f1sh-d0g@users.noreply.github.com>
2024-11-19 19:01:53 +0300

    Меняю код на gitHub

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Пока 03?");
+        System.out.println("Какая-то надпись");
+        System.out.println("Другая надпись");
     }
 }
